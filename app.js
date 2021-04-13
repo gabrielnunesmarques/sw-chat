e94916d83495e96b92a572033e4efaabcfe01c60
gabrielnunesmarques <71200846+gabrielnunesmarques@users.noreply.github.com>
2021-04-12 22:23:30 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById("btnSend");
+document.getElementById("btnSend").onclick = function()
+{
+    var textuser = document.getElementById("userchat3");
+
+    console.log(textuser.value);
+}
